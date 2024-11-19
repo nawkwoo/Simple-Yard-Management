ed1fbaf5e7c6b8f178413e8a3aedce292c6088f4
nawkwoo <sonkw4775@naver.com>
2024-11-19 10:54:16 +0900

    [fix] migrate 오류 수정

--- apps/yms_edit/apps.py
+++ apps/yms_edit/apps.py
@@ -6,19 +6,15 @@ class YmsEditConfig(AppConfig):
     name = "apps.yms_edit"
 
     def ready(self):
-        from django.db import connection
         from .models import Division
-        
-        # post_migrate 시그널 연결
         post_migrate.connect(self.create_default_divisions, sender=self)
 
     @staticmethod
     def create_default_divisions(sender, **kwargs):
+        """기본 Division 데이터를 생성"""
         from .models import Division
-        
-        if 'default' in sender.apps.get_app_configs():
-            try:
-                Division.create_default_divisions()
-                print("기본 Division이 성공적으로 생성되었습니다.")
-            except Exception as e:
-                print(f"기본 Division 생성 중 오류 발생: {e}")
+        try:
+            Division.create_default_divisions()
+            print("기본 Division이 성공적으로 생성되었습니다.")
+        except Exception as e:
+            print(f"기본 Division 생성 중 오류 발생: {e}")
